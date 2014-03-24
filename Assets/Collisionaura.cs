21fdc6bf4d3bf1459e321f4a032f5fe6114e587a
achaibiisart <achaibimer@gmail.com>
2014-03-24 23:16:06 +0100

    collision aura
    collision avec aura prises en compte

---
+++ Assets/Collisionaura.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collisionaura : MonoBehaviour {
+	rockete script;
+	// Use this for initialization
+	void Start () {
+		script=GameObject.Find("viseearme").GetComponent<rockete>();
+
+	}
+	
+	void OnCollisionEnter(Collision other){
+	if (other.gameObject.tag=="enncube"&& script.randomnum==1)
+		{
+				Debug.Log("pasaiecube");
+			script.selection+=3;
+		}
+	if (other.gameObject.tag=="ennsphere"&& script.randomnum==0)
+			{
+				Debug.Log("pasaisphere");
+						script.selection+=2;
+
+		}
+		if (other.gameObject.tag=="enncyl"&& script.randomnum==2)
+			{
+				Debug.Log("pasaiecyl");	
+						script.selection+=3;
+
+		}
+	}
+		
+	// Update is called once per frame
+	void Update () {
+	}
+}
